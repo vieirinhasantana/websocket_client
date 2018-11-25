83cb4a1f668deaff4530d67625edd0ff2cdec16a
Daniel Santana <vieirinhasantana@gmail.com>
2018-11-25 16:25:36 -0200

    File

--- src/components/EditValuesFields.vue
+++ src/components/EditValuesFields.vue
@@ -1,2 +1,43 @@
 <template>
+  <b-container class="bv-example-row">
+    <div>
+      <form @submit="onSubmit">
+        <b-form-select v-model="form.name" :options="options" class="mb-3" />
+        <b-form-input type="text" v-model="form.share_value" required placeholder="Enter value"></b-form-input>
+        <b-button type="submit" variant="primary">Atualizar</b-button>
+      </form>
+      <b-alert variant="success" :show="showDismissibleAlert">Success update</b-alert>
+    </div>
+  </b-container>
 </template>
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      showDismissibleAlert: false,
+      form: {
+        name: '',
+        share_value: ''
+      },
+      options: [
+        { value: null, text: 'Please select an option' },
+        { value: 'PETR4', text: 'PETR4' },
+        { value: 'QUAL3', text: 'QUAL3' },
+        { value: 'RADL3', text: 'RADL3' },
+        { value: 'RAIL3', text: 'RAIL3' },
+        { value: 'RENT3', text: 'RENT3' }
+      ]
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      evt.preventDefault()
+      let payload = JSON.stringify(this.form)
+      axios.get(`http://127.0.0.1:3000/send_data`, {payload}).then((res) => {
+        this.showDismissibleAlert = true
+      })
+    }
+  }
+}
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,43 @@
 <template>
-  <b-table striped hover :items="items"></b-table>
+  <b-container class="bv-example-row">
+    <div class="row">
+      <b-table striped hover :items="items" :fields="fields"></b-table>
+      <router-link to="edit">Editar valores</router-link>
+    </div>
+  </b-container>
 </template>
 <script>
 export default {
   data () {
     return {
+      fields: [
+        {
+          key: 'name',
+          label: 'Nome',
+          sortable: true
+        },
+        {
+          key: 'share_value',
+          label: 'Preço (R$)',
+          sortable: true
+        }
+      ],
       items: []
     }
   },
   mounted () {
-    this.$options.sockets.onmessage = (wsResponse) => {
-      let jsonObj = JSON.parse(wsResponse.data)
-      this.items = jsonObj.payload
+    this.socketLoad()
+  },
+  methods: {
+    socketLoad () {
+      this.$options.sockets.onmessage = (wsResponse) => {
+        let jsonObj = JSON.parse(wsResponse.data)
+        this.items = jsonObj.payload
+      }
     }
+  },
+  beforeDestroy () {
+    clearInterval(this.socketLoad)
   }
 }
 </script>
