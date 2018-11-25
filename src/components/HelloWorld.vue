85e643c15d2f32e3ea0c766a588345111f3bb1a9
Daniel Santana <vieirinhasantana@gmail.com>
2018-11-25 10:38:43 -0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-table striped hover :items="items"></b-table>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      items: []
+    }
+  },
+  mounted () {
+    this.$options.sockets.onmessage = (wsResponse) => {
+      let jsonObj = JSON.parse(wsResponse.data)
+      this.items = jsonObj.payload
+    }
+  }
+}
+</script>
